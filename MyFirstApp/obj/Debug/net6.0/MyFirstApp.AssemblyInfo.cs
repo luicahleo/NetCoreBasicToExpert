ca20d90a50cf644513e1f156c0425268df19c34a
luisr <luisr@DESKTOP-4NLA5R0>
2024-04-19 00:12:29 +0200

    kestrel

--- MyFirstApp/obj/Debug/net6.0/MyFirstApp.AssemblyInfo.cs
+++ MyFirstApp/obj/Debug/net6.0/MyFirstApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyFirstApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d40efc0537a7a2dd4ba98c016fb137ad3dd4f0b2")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyFirstApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyFirstApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
